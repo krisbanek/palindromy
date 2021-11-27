768d51c98a256e9b1de44b7927a5d93149f10c66
Krzysztof Banek <krisbanek@gmail.com>
2021-11-27 20:45:33 +0100

    Add lower and replace and print

--- palindromy.py
+++ palindromy.py
@@ -1,12 +1,14 @@
-#palindrom np. kajak, kobyła ma mały bok
+#palindrome for example: kajak, kobyła ma mały bok
+
+word = "Kobyła ma mały bok"
 def palindrom(word):
-    n = len(word)
-    #zmienna zachwująca długość słowa
-    for i in range(n - 1):
-        #n -1 ponieważ działamy na indeksach
+    word = word.lower()          #Capital letters doen't matter any more.
+    word = word.replace(" ", "") # removing the spaces so sentences ca be checked.
+    n = len(word)                #calculating lenght of given word.
+    for i in range(n - 1):       #n -1 due to switching to index domain.
         if word[i] != word[n - 1 - i]:
             return False
     return True
-    
-
-print(palindrom("Kajak"))
+print(f"Is {word} a palindrome?")
+result = palindrom(word)
+print(result)
