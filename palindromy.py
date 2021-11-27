2030214a775f2026ee8dee5cf7ec6dbaec5190b6
Krzysztof Banek <krisbanek@gmail.com>
2021-11-27 19:58:12 +0100

    Add loop for and if for palindrome detection

--- palindromy.py
+++ palindromy.py
@@ -1,8 +1,12 @@
 #palindrom np. kajak, kobyła ma mały bok
 def palindrom(word):
-    i = len(word)
-
+    n = len(word)
+    #zmienna zachwująca długość słowa
+    for i in range(n - 1):
+        #n -1 ponieważ działamy na indeksach
+        if word[i] != word[n - 1 - i]:
+            return False
+    return True
     
-    print(i)
 
-palindrom("kajak")
+print(palindrom("Kajak"))
